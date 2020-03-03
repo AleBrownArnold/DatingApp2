d3916695bb9780ffef600f0c21a948eeaf44b0a1
Alejandro <charlie14_1939@hotmail.com>
2020-03-03 13:51:32 -0300

    Lesson 28 Repository Register Method

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+
+            CreatePasswordHash(password, out passwordHash, out passwordSalt); //Al agregar el "out" al método se le pasa una referencia a la variable, de esta forma la variable se modifica en todos los niveles
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) //todo lo que se use dentro de los corchetes se descartará una vez se termine
+            {
+                passwordSalt = hmac.Key; // Esta key se genera aleatoria
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
