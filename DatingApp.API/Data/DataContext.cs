10bc1b52c6e56aa5dab92342504c61c32ed85ffd
Alejandro <charlie14_1939@hotmail.com>
2020-03-03 13:27:17 -0300

    Lesson 26 User Model

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
 
         public DbSet<Value> Values { get; set; } // The name of these property is the name that the table will have
+        public DbSet<User> Users{get;set;}
     }
 }
