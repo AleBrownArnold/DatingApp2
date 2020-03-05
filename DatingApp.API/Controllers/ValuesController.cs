38e9734925a2dd27d67c58a15831b9a440714206
Alejandro <charlie14_1939@hotmail.com>
2020-03-05 15:40:18 -0300

    Lesson 46 del value comp, comunn between component

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -23,7 +23,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
-        [Authorize]
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
