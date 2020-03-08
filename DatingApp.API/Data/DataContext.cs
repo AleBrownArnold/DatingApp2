b0e667d5d8a7278a12b376f28e5796b3202d3f53
Alejandro <charlie14_1939@hotmail.com>
2020-03-08 19:25:24 -0300

    Lesson 70 UserExtended first migration and photos

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Data
 
         public DbSet<Value> Values { get; set; } // The name of these property is the name that the table will have
         public DbSet<User> Users{get;set;}
+        public DbSet<Photo> Photos { get; set; }
     }
 }
