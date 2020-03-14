233813987fbfec475bedbbe6567dacb9e6c16dda
Alejandro <charlie14_1939@hotmail.com>
2020-03-14 13:12:32 -0300

    Lesson 102 Persisting changes. End section 10

--- DatingApp.API/DTOs/UserForUpdateDto.cs
+++ DatingApp.API/DTOs/UserForUpdateDto.cs
@@ -4,11 +4,8 @@ namespace DatingApp.API.DTOs
     {
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
-
         public string Interests { get; set; }
-
         public string City { get; set; }
-
         public string Country { get; set; }
 
     }
