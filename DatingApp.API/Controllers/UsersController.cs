1b2778f455aba2f6fa4dcf622ddb73d5d5d0308d
Alejandro <charlie14_1939@hotmail.com>
2020-03-19 16:36:17 -0300

    Lesson 145 filter action

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -29,6 +29,17 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
+            var cuerrentUserID = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var userFromRepo = await _repo.GetUser(cuerrentUserID);
+
+            userParams.UserId = cuerrentUserID;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
+
             var users = await _repo.GetUsers(userParams);
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
