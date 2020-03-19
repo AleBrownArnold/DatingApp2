67c5364d128e8cedf78e0cca5e11abb28e627f37
Alejandro <charlie14_1939@hotmail.com>
2020-03-19 12:10:06 -0300

    Lesson 142 Implementation Pagination API

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -3,13 +3,13 @@ namespace DatingApp.API.Helpers
     public class UserParams
     {
         private const int MaxPageSize = 50;
-        public int pageNumber { get; set; } = 1; //default value 1
+        public int PageNumber { get; set; } = 1; //default value 1
         private int pageSize = 10; //propfull
         public int PageSize
         {
-            get { return pageSize = 10; }
+            get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
-        
+
     }
 }
