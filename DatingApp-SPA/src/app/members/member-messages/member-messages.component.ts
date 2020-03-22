0c493a30c2c23187d93150e1402223f5a29881d8
Alejandro <charlie14_1939@hotmail.com>
2020-03-22 15:27:29 -0300

    Lesson 175 Mark Read Funcionality

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -4,6 +4,8 @@ import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { error } from 'protractor';
+import { tap } from 'rxjs/operators';
+import { MessagesResolver } from 'src/app/_resolvers/messages.resolver';
 
 @Component({
   selector: 'app-member-messages',
@@ -24,7 +26,17 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid; // the + is to force nameid to be a number
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (const message of messages) {
+            if (message.isRead === false && message.recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, message.id);
+            }
+          }
+        })
+      )
       .subscribe(messages => {
         this.messages = messages;
       }, err => {
