357eccc6ffb687bfae147d8990cbd2158fef5d79
Alejandro <charlie14_1939@hotmail.com>
2020-03-24 12:03:06 -0300

    Lesson 101 Fixed Errors

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -15,11 +15,11 @@ namespace DatingApp.API.Data
         public async Task<User> Login(string username, string password)
         {
             var user = await _context.Users.Include(p => p.Photos) //a collection is not automatically returned
-            .FirstOrDefaultAsync(x => x.Username == username);
+            .FirstOrDefaultAsync(x => x.UserName == username);
 
             if (user == null) return null;
 
-            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) return null;
+            //if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) return null;
             
             return user;
         }
@@ -44,8 +44,8 @@ namespace DatingApp.API.Data
 
             CreatePasswordHash(password, out passwordHash, out passwordSalt); //Al agregar el "out" al método se le pasa una referencia a la variable, de esta forma la variable se modifica en todos los niveles
 
-            user.PasswordHash = passwordHash;
-            user.PasswordSalt = passwordSalt;
+            // user.PasswordHash = passwordHash;
+            // user.PasswordSalt = passwordSalt;
 
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
@@ -65,7 +65,7 @@ namespace DatingApp.API.Data
 
         public async Task<bool> UserExists(string username)
         {
-            if (await _context.Users.AnyAsync(x => x.Username == username)) return true;
+            if (await _context.Users.AnyAsync(x => x.UserName == username)) return true;
             return false;
         }
     }
