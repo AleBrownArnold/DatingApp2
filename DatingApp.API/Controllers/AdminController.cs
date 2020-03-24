993568dcaa664c5940a748edb66c490c27c1bbe7
Alejandro <charlie14_1939@hotmail.com>
2020-03-24 15:51:20 -0300

    Lesson 208 Retrieving users identity-role model

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,14 +11,34 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
-        [Authorize(Policy="RequireAdminRole")]
-        [HttpGet("usersWithRoles")]
-        public IActionResult GetUserWithRoles() {
-            return Ok("Only admins can see this");
+        private readonly DataContext _context;
+        public AdminController(DataContext context)
+        {
+            _context = context;
         }
-        [Authorize(Policy="ModeratePhotoRole")]
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("usersWithRoles")]
+        public async Task<IActionResult> GetUserWithRoles()
+        {
+            var userList = await _context.Users
+            .OrderBy(x => x.UserName)
+            .Select(user => new {
+                Id = user.Id,
+                UserName = user.UserName,
+                Roles = (from userRole in user.UserRoles
+                            join role in _context.Roles
+                            on userRole.RoleId
+                            equals role.Id
+                            select role.Name).ToList()
+            }).ToListAsync();
+
+            return Ok(userList);
+        }
+        [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photosForModeration")]
-        public IActionResult GetPhotosForModeration() {
+        public IActionResult GetPhotosForModeration()
+        {
             return Ok("Admins or moderators can see this");
         }
     }
