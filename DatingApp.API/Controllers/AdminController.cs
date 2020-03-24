baa5bc545112a21b3fb1483314f00fbf5add93d5
Alejandro <charlie14_1939@hotmail.com>
2020-03-24 14:05:55 -0300

    Lesson 207 Policy Driven Controllers

---
+++ DatingApp.API/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        [Authorize(Policy="RequireAdminRole")]
+        [HttpGet("usersWithRoles")]
+        public IActionResult GetUserWithRoles() {
+            return Ok("Only admins can see this");
+        }
+        [Authorize(Policy="ModeratePhotoRole")]
+        [HttpGet("photosForModeration")]
+        public IActionResult GetPhotosForModeration() {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+}
