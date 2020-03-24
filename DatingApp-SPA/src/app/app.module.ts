36ddfc2f24c350b5aab30c485aebae97f7d2da10
Alejandro <charlie14_1939@hotmail.com>
2020-03-24 17:12:04 -0300

    Lesson 213 Admin Panel User Management

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -34,6 +34,9 @@ import { MessagesResolver } from './_resolvers/messages.resolver';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/hasRole.directive';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { AdminService } from './_services/admin.service';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -62,7 +65,9 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       TimeAgoPipe,
       MemberMessagesComponent,
       AdminPanelComponent,
-      HasRoleDirective
+      HasRoleDirective,
+      PhotoManagementComponent,
+      UserManagementComponent
    ],
    imports: [
       BrowserModule,
@@ -98,7 +103,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig},
       PreventUnsavedChanges,
       ListsResolver,
-      MessagesResolver
+      MessagesResolver,
+      AdminService
    ],
    bootstrap: [
       AppComponent
